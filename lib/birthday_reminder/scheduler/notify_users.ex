8a4dd5772bdec21cbc44ef1ce4df12b5266b6851
Vlad Romaniuk <romanuk_v@live.ru>
2020-09-23 19:52:51 +0300

    fix all warnings

--- lib/birthday_reminder/scheduler/notify_users.ex
+++ lib/birthday_reminder/scheduler/notify_users.ex
@@ -6,8 +6,8 @@ defmodule BirthdayReminder.NotifyUsers do
   @payment_limit 5000
 
   def run do
-    case birthday_boys do
-      [%User{}|_] -> send_reminders
+    case birthday_boys() do
+      [%User{}|_] -> send_reminders()
       [] -> nil
     end
   end
@@ -21,13 +21,13 @@ defmodule BirthdayReminder.NotifyUsers do
   end
 
   defp birthday_boy_names do
-    birthday_boys
+    birthday_boys()
     |> Enum.map(fn user -> "#{user.first_name} #{user.last_name}" end)
     |> Enum.join(", ")
   end
 
   defp send_reminders do
-    birthday_boys
+    birthday_boys()
     |> Users.subscribed_users
     |> Enum.each(&send_reminder(&1))
   end
@@ -37,16 +37,16 @@ defmodule BirthdayReminder.NotifyUsers do
   end
 
   defp payment_size do
-    round(@payment_limit / Enum.count(Users.subscribed_users(birthday_boys))) * Enum.count(birthday_boys)
+    round(@payment_limit / Enum.count(Users.subscribed_users(birthday_boys()))) * Enum.count(birthday_boys())
   end
 
   defp text_message(user) do
     """
     Hello #{user.first_name},
 
-    #{Timex.format!(birthday, "%d %B", :strftime)} is *#{birthday_boy_names}'s* birthday.
+    #{Timex.format!(birthday(), "%d %B", :strftime)} is *#{birthday_boy_names()}'s* birthday.
 
-    Send #{payment_size} uah to the card
+    Send #{payment_size()} uah to the card
 
     5168 7554 2585 9853
     """
