20288778cb3665733d99e7a2a8fd054df6991aad
drEnilight <vladyslav.romaniuk@outlook.com>
2021-05-22 18:00:44 +0300

    update deps versions

--- mix.exs
+++ mix.exs
@@ -5,12 +5,13 @@ defmodule BirthdayReminder.MixProject do
     [
       app: :birthday_reminder,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      preferred_cli_env: [check: :test]
     ]
   end
 
@@ -25,7 +26,7 @@ defmodule BirthdayReminder.MixProject do
   end
 
   # Specifies which paths to compile per environment.
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(:test), do: ["lib", "test/support", "test/factories"]
   defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
@@ -33,23 +34,45 @@ defmodule BirthdayReminder.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.6"},
+      # phoenix
+      {:phoenix, "~> 1.5.7"},
       {:phoenix_ecto, "~> 4.1"},
       {:ecto_sql, "~> 3.4"},
-      {:postgrex, ">= 0.0.0"},
+      {:phoenix_live_view, "~> 0.15.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.14.6"},
-      {:phoenix_live_dashboard, "~> 0.3 or ~> 0.2.9"},
+
+      # live dashboard and telemetry
+      {:phoenix_live_dashboard, "~> 0.4"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
+
+      # internationalization and localization
       {:gettext, "~> 0.11"},
+
+      # JSON parser
       {:jason, "~> 1.0"},
+
+      # adapter to cowboy web server
       {:plug_cowboy, "~> 2.0"},
+
+      # date/time library
+      {:timex, "~> 3.6"},
+
+      # database adapter
+      {:postgrex, ">= 0.0.0"},
+
+      # Telegram Bot API Wrapper
       {:nadia, "~> 0.7.0"},
+
+      # Cron-like job scheduler
       {:quantum, "~> 3.0"},
-      {:timex, "~> 3.5"},
-      {:credo, "~> 1.4", only: [:dev, :test], runtime: false}
+
+      # Static code analyzer
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+
+      # create test data
+      {:ex_machina, "~> 2.5.0", only: :test}
     ]
   end
 
